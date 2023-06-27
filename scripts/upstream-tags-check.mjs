95666a187fe4e568a2b6bdc2277f1bca0f2a9c3d
yaegassy <yaegassy@users.noreply.github.com>
2023-06-27 15:58:40 +0900

    chore(ci): add tags-check

---
+++ scripts/upstream-tags-check.mjs
@@ -0,0 +1,24 @@
+import fs from 'node:fs';
+
+const FEATCH_URL = 'https://api.github.com/repos/microsoft/vscode-black-formatter/tags';
+const RESOURCE_FILE_NAME = 'vscode-black-formatter-tags.json';
+
+await (async () => {
+  const res = await fetch(FEATCH_URL);
+  if (!res.ok) return;
+  const remoteJson = await res.json();
+  const remoteJsonStr = JSON.stringify(remoteJson, null, 2);
+
+  const resourcePath = new URL(`../resources/${RESOURCE_FILE_NAME}`, import.meta.url).pathname;
+  let localJsonStr = await fs.promises.readFile(resourcePath, { encoding: 'utf-8' });
+  const localJson = JSON.parse(localJsonStr);
+  localJsonStr = JSON.stringify(localJson, null, 2);
+
+  if (remoteJsonStr === localJsonStr) {
+    console.log('OK');
+    process.exit(0);
+  } else {
+    console.log('NG');
+    process.exit(1);
+  }
+})();
